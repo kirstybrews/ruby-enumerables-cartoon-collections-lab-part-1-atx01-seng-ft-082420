68f00324b20277aeb36e9b65c6459b9918d72e8a
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-08-12 19:17:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |index, item|
-    p "#{index + 1}. #{element}"
+    p "#{index + 1}. #{item}"
   end
 end
